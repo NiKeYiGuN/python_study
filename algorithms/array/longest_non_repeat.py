b10b6d1c6c4e32efcdead52670f98fcf6abb69aa
linzhenhao <linzhenhao1995@gmail.com>
2022-08-08 17:08:14 +0800

    longest_non_repeat_sub_string

--- algorithms/array/longest_non_repeat.py
+++ algorithms/array/longest_non_repeat.py
@@ -8,46 +8,96 @@ Given "pwwkew", the answer is "wke", with the length of 3.
 Note that the answer must be a substring,
 "pwke" is a subsequence and not a substring.
 """
+import random
+import string
+from collections import defaultdict
+
 from util.time_util import timer
 
 
 @timer
-def longest_non_repeat_v1(string: str) -> None:
+def longest_non_repeat_v1(chars: str) -> int:
     """
     Violent method
     """
     max_sub_len = 0
 
-    string_len = len(string)
+    string_len = len(chars)
     for i in range(string_len):
         for j in range(string_len - i):
-            if len(set(string[i : i + j + 1])) != len(string[i : i + j + 1]):
+            if len(set(chars[i : i + j + 1])) != len(chars[i : i + j + 1]):
                 continue
-            max_sub_len = max(max_sub_len, len(string[i : i + j + 1]))
+            max_sub_len = max(max_sub_len, len(chars[i: i + j + 1]))
 
     print("The longest substring length:", max_sub_len)
+    return max_sub_len
 
 
 @timer
-def longest_non_repeat_v2(string: str) -> None:
+def longest_non_repeat_v2(chars: str) -> int:
     max_sub_len = 0
+    string_len = len(chars)
 
-    string_len = len(string)
     for i in range(string_len):
         sub_len = 0
         sub_string = set()
-        for j in range(i, string_len):
-            if string[j] in sub_string:
-                sub_len = j - i
+        for j in range(i + 1, string_len):
+            if chars[j] in sub_string or j == string_len - 1:
+                sub_len = j - i-1
                 break
             else:
-                sub_string.add(string[j])
+                sub_string.add(chars[j])
 
         max_sub_len = max(sub_len, max_sub_len)
 
     print("The longest substring length:", max_sub_len)
+    return max_sub_len
+
+
+@timer
+def longest_non_repeat_v3(chars: str) -> int:
+    right, left = 0, 0
+    times_map = defaultdict(int)
+    max_sub_len = 0
+    string_len = len(chars)
+
+    for right in range(string_len):
+        times_map[chars[right]] += 1
+        while times_map[chars[right]] > 1:
+            times_map[chars[left]] -= 1
+            left += 1
+
+        max_sub_len = max(max_sub_len, right - left + 1)
+
+    print("The longest substring length:", max_sub_len)
+    return max_sub_len
+
+
+@timer
+def longest_non_repeat_v4(chars: str) -> int:
+    right, left = 0, 0
+    index_map = {}
+    max_sub_len = 0
+    string_len = len(chars)
+
+    for right in range(string_len):
+        if chars[right] in index_map and index_map[chars[right]] >= left:
+            left = index_map[chars[right]] + 1
+
+        index_map[chars[right]] = right
+        max_sub_len = max(max_sub_len, right - left + 1)
+
+    print("The longest substring length:", max_sub_len)
+    return max_sub_len
 
 
 if __name__ == "__main__":
-    longest_non_repeat_v1("abcabcb")
-    longest_non_repeat_v2("abcabcbb")
+    letters = ""
+
+    for i in range(2000):
+        letters += random.choice(string.ascii_lowercase)
+
+    longest_non_repeat_v1(letters)
+    longest_non_repeat_v2(letters)
+    longest_non_repeat_v3(letters)
+    longest_non_repeat_v4(letters)
--- test/algorithms/array/test_array.py
+++ test/algorithms/array/test_array.py
@@ -1,6 +1,10 @@
+import random
+import string
+
 from algorithms.array.delete_nth import delete_nth_native, delete_nth
 from algorithms.array.flatten import flatten, flatten_iter
 from algorithms.array.josephus import josephus, josephus_yield
+from algorithms.array.longest_non_repeat import longest_non_repeat_v1, longest_non_repeat_v2, longest_non_repeat_v3, longest_non_repeat_v4
 
 
 def test_delete_nth_navite():
@@ -29,3 +33,13 @@ def test_josephus():
 
 def test_josephus_yield():
     assert list(josephus_yield([1, 2, 3, 4, 5, 6, 7, 8, 9])) == [3, 6, 9, 4, 8, 5, 2, 7, 1]
+
+
+def test_longest_non_repeat():
+    chars = ""
+
+    for i in range(1000):
+        chars += random.choice(string.ascii_lowercase)
+
+    assert longest_non_repeat_v1(chars) == longest_non_repeat_v2(chars)
+    assert longest_non_repeat_v3(chars) == longest_non_repeat_v4(chars)
